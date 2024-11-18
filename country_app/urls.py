4669cdd9a8e2a0980b6d79fc246554a5758dba53
AAF77 <87871137+AAF77@users.noreply.github.com>
2024-11-18 21:49:46 +0300

    convert into deploying mode

--- country_app/urls.py
+++ country_app/urls.py
@@ -1,8 +1,8 @@
 # File: country_app/urls.py
 
 from django.urls import path
-from country_app.views import CountrySearchView
+from country_app.views import CountriesView
 
 urlpatterns = [
-   path('countries/', CountrySearchView.as_view(), name='unique-country-list'),
+   path('countries/', CountriesView.as_view(), name='unique-country-list'),
 ]
